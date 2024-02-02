c77ca3c2d84fe0ee06217d82eabda4b50f74d3f7
ErnstOsbeck <143172129+ErnstOsbeck@users.noreply.github.com>
2024-02-02 12:55:00 +0100

    Add project files.

---
+++ Inlämningsuppgift 7.1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace uppgift_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> Namn = new List<string>();
+            Console.WriteLine("Skriv in vafritt antal namn");
+            Console.WriteLine("avsluta genom att skriva en blank rad");
+            string namn = "x";
+            while (namn != "")
+            {
+                namn = Console.ReadLine();
+                Namn.Add(namn);
+            }
+            Namn.Sort();
+            Namn.Reverse();
+            foreach(string element in Namn)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
